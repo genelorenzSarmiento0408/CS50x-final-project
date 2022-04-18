06d4735d8196a12fba8cff37c1122d34b02cc019
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2022-04-18 13:59:45 +0800

    test again

--- app/app.py
+++ app/app.py
@@ -19,6 +19,10 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
+from app.controller import pwa
+app.register_blueprint(pwa.bp)
+
 def apology(m, code):
     return render_template("apology.html", error=m), code
 
